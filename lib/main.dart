01c8c4d9a7d701c5fc633af624df5a53474c49f5
Sehajbir <sehajbirsingh97@gmail.com>
2019-09-30 04:05:07 +0530

    Added Weight Selection Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() => runApp(MyApp());
 
@@ -110,29 +109,161 @@ class Page1 extends StatefulWidget{
 }
 
 class _Page1State extends State <Page1>{
+  var weight = 40.0;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
     return Scaffold(
       backgroundColor: Colors.green,
       body: Center(
         child: Column(
           children: <Widget>[
             new Expanded(child: Container()),
+            Center(
+              child: Text("WEIGHT (Kg)", style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20, fontFamily: 'Open Sans', color: Colors.white),),
+            ),
             Padding(
-              padding: const EdgeInsets.only(bottom: 40.0),
+              padding: const EdgeInsets.only(bottom: 50.0),
+              child: Center(
+                child: Text(weight.toString(), style: TextStyle(fontWeight: FontWeight.w900, fontSize: 48, fontFamily: 'Open Sans', color: Colors.white),),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
               child: new Container(
                 width: double.infinity,
-                height: 80,
-                color: Colors.red,
+                height: 60,
+                color: Colors.transparent,
                 child: Row(
+                  children: <Widget>[
+                    new Expanded(child: Container()),
+                    SliderTheme(
+                      data: SliderThemeData(
+                        trackHeight: 20,
+                        inactiveTrackColor: Colors.white,
+                        thumbColor: Colors.white.withOpacity(1.0),
+                        thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0, disabledThumbRadius: 20.0)
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: Container(
+                          width: 300,
+                          child: new Slider(
+                              value: weight,
+                              min: 30,
+                              max: 150,
+                              divisions: 120,
+                              activeColor: Colors.white.withOpacity(0.8),
+                              inactiveColor: Colors.white.withOpacity(0.2),
+                              onChanged: (_value) {
+                                setState(() {
+                                  weight = _value;
+                                });
+                              },
 
+                          ),
+                        ),
+                      ),
+                    ),
+                    new Expanded(child: Container())
+                  ],
                 ),
               ),
+            ),
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 30.0, left: 40.0),
+                  child: new Container(
+                    width: 90,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 1),
+                      borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                      color: Colors.white.withOpacity(0.2),
+                    ),
+//                    color: Colors.white.withOpacity(0.2),
+                    child: new IconButton(icon: Icon(Icons.arrow_back_ios, color: Colors.white,), onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => MyHomePage()));
+                    }),
+                  ),
+                ),
+                new Expanded(child: Container()),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 30.0, right: 0.0),
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: new Container(
+                          width: 180,
+                          height: 55,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.all(Radius.circular(40.0))
+                          ),
+                          child: new Center(
+                            child: Text("Next", style: TextStyle(fontSize: 20, color: Colors.green, fontFamily: 'Roboto', fontWeight: FontWeight.bold), ),
+                          )
+                      ),
+
+                      onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => HeightPage()));
+                        },
+                      ),
+//                      new Container(
+//                          width: 200,
+//                          height: 55,
+//                          decoration: BoxDecoration(
+//                              color: Colors.white,
+//                              borderRadius: BorderRadius.all(Radius.circular(40.0))
+//                          ),
+//                          child: new Center(
+//                            child: Text("Next", style: TextStyle(fontSize: 20, color: Colors.green, fontFamily: 'Roboto', fontWeight: FontWeight.bold), ),
+//                          )
+//                      ),
+
+                    ],
+                  ),
+                ),
+
+              ],
             )
           ],
         ),
       )
+    );
+  }
+
+}
+
+class HeightPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: heightPage(),
+    );
+  }
+}
+
+// ignore: camel_case_types
+class heightPage extends StatefulWidget{
+  @override
+  _HeightPage createState() => _HeightPage();
+
+}
+
+class _HeightPage extends State <heightPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+      ),
     );
   }
 
