1a38439e6229a716f187b9f17b42a48303b96885
Sehajbir <sehajbirsingh97@gmail.com>
2019-10-05 14:33:11 +0530

    Added more animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 void main() => runApp(MyApp());
 
 class BMI{
   double weight;
   double height;
+  double result;
+  var cat;
+  Color col;
   BMI(){
     weight = 40;
     height = 150;
+    result = 0.0;
+    cat = "";
   }
   
   void setWeight(var a){
@@ -17,6 +23,41 @@ class BMI{
   
   void setHeight(var a){
     height = a;
+  }
+  void set1(){
+    weight = 40;
+    height = 150;
+    result = 0.0;
+    cat = "";
+  }
+  void set(var a){
+      result = a;
+  }
+  void eval(){
+    if(result < 18.5){
+      cat = "Underweight";
+      col = Colors.blueAccent;
+    }
+    else if(result < 24.9) {
+      cat = "Normal";
+      col = Colors.green;
+    }
+    else if(result < 29.9) {
+      cat = "Overweight";
+      col = Colors.redAccent;
+    }
+    else if(result < 34.9){
+      cat = "Obese I";
+      col = Colors.redAccent;
+    }
+    else if(result < 39.9){
+      cat = "Obese II";
+      col = Colors.redAccent;
+    }
+    else{
+      cat = "Extreme Obesity";
+      col = Colors.red;
+    }
   }
 }
 
@@ -32,6 +73,7 @@ class MyApp extends StatelessWidget {
       ),
       home: MyHomePage(),
       routes: <String, WidgetBuilder>{
+        '/home' : (BuildContext context) => MyHomePage(),
         '/weight' : (BuildContext context) => Page1(),
         '/height' : (BuildContext context) => heightPage()
       }
@@ -46,6 +88,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomepageState extends State<MyHomePage> {
+  var r = obj.result;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -91,7 +134,7 @@ class _MyHomepageState extends State<MyHomePage> {
               ),
             ),
             new Expanded(child: Container()),
-            Text("Launch the Rocket to reach the skies", style: TextStyle(fontSize: 12),),
+            Text("Launch the Rocket to reach the skies", style: TextStyle(fontSize: 14),),
             Padding(
               padding: const EdgeInsets.only(bottom: 2.0),
               child: new Draggable(
@@ -118,6 +161,7 @@ class Page1 extends StatefulWidget{
 }
 
 class _Page1State extends State <Page1>{
+  var r = obj.result;
   var weight = obj.weight;
   @override
   Widget build(BuildContext context) {
@@ -129,7 +173,13 @@ class _Page1State extends State <Page1>{
           children: <Widget>[
             new Expanded(child: Container()),
             Center(
-              child: Text("WEIGHT (Kg)", style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20, fontFamily: 'Open Sans', color: Colors.white),),
+              child: Image.asset('images/weight.png', scale: (0.5 * (150/obj.weight))),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 40.0),
+              child: Center(
+                child: Text("WEIGHT (Kg)", style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20, fontFamily: 'Open Sans', color: Colors.white),),
+              ),
             ),
             Padding(
               padding: const EdgeInsets.only(bottom: 50.0),
@@ -265,7 +315,13 @@ class _HeightPage extends State <heightPage>{
           children: <Widget>[
             new Expanded(child: Container()),
             Center(
-              child: Text("HEIGHT (cm)", style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20, fontFamily: 'Open Sans', color: Colors.white),),
+              child: Image.asset('images/height.png', scale: 0.5 * (240 / obj.height),),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top :40.0),
+              child: Center(
+                child: Text("HEIGHT (cm)", style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20, fontFamily: 'Open Sans', color: Colors.white),),
+              ),
             ),
             Padding(
               padding: const EdgeInsets.only(bottom: 50.0),
@@ -353,7 +409,11 @@ class _HeightPage extends State <heightPage>{
                         ),
 
                         onPressed: () {
-
+                          var r = obj.weight / (obj.height * obj.height);
+                          r = r * 10000;
+                          obj.set(r);
+                          obj.eval();
+                          Navigator.push(context, CupertinoPageRoute(builder: (context) => ResultPage()));
                         },
                       ),
 
@@ -365,6 +425,81 @@ class _HeightPage extends State <heightPage>{
             )
           ],
         ),
+      )
+    );
+  }
+}
+
+class ResultPage extends StatefulWidget{
+  @override
+  _ResultsPage createState() => _ResultsPage();
+
+}
+
+class _ResultsPage extends State <ResultPage>{
+  var r = obj.result.toString();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: obj.col,
+      body: Stack(
+        children: <Widget>[
+          FlareActor(
+            "images/clouds.flr",
+            animation: "movement",
+            
+          ),
+          Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top : 220),
+                child: Center(
+                    child: Text(r.substring(0,4), style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 80,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),)
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: new Container(
+                  child: new Text(obj.cat, style: TextStyle(fontSize: 22, color: Colors.white, fontWeight: FontWeight.bold),),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 200,
+                color: Colors.transparent,
+              ),
+              new Expanded(child: Container()),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                    border: Border.all(color: Colors.black54, width: 2.0)
+                  ),
+                  child: IconButton(
+                      icon: Icon(
+                        Icons.refresh,
+                        color: Colors.black54,
+                        size: 30,
+                      ),
+                      onPressed: () {
+                          obj.set1();
+                          Navigator.popUntil(context, ModalRoute.withName('/'));
+                      }
+                  ),
+                )
+
+              )
+            ],
+          )
+        ],
       )
     );
   }
